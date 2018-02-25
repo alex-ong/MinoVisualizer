f03226dc236f8384899b19df28d184043e43e9aa
alex-ong <the.onga@gmail.com>
2018-02-25 17:05:57 +1100

    First build

--- Assets/Scripts/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class CONSTANTS {
-
-    public static int FIELD_HEIGHT = 20;
-    public static int FIELD_WIDTH = 10;
-}
